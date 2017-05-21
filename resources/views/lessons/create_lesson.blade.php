5dc556c603e49a69f13f65eee766e576cbb62578
nikol pein <pascasionicoleb@gmail.com>
2017-05-21 20:41:21 +0800

    Create a lesson - Static Page

--- resources/views/lessons/create_lesson.blade.php
+++ resources/views/lessons/create_lesson.blade.php
@@ -5,73 +5,75 @@
     <div class="row">
         <form action="" method="">
             <div class="col-md-8 col-md-offset-2">
-                <h1>Create a Lesson for [Math]</h1>
+                <!-- <h1>Create a Lesson for [Math]</h1> -->
 
                 <div class="form-group">
 
-                    <div class="panel panel-default">
+                    <div class="panel panel-default custom-panel">
                         <div class="panel-body">
-                            <div class="input-group center-block text-center">
-                                <input type="text" class="form-control text-center" name="title" placeholder="Title of your lesson" />
+                            <div class="col-md-8 input-group center-block text-center">
+                                <input type="text" class="form-control text-center custom-form text-uppercase" name="title" placeholder="LESSON TITLE" />
                             </div>
                         </div>
                     </div>
                                     
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
+                    <div class="panel panel-default custom-sub-panel">
+                        <div class="panel-heading custom-heading">
                             <div class="col-md-6 pull-left center-block">
-                                <input type="text" class="form-control text-center" name="pageTitle" placeholder="Title of your Page" />
+                                <input type="text" class="form-control custom-sub-form" name="pageTitle" placeholder="Provide a Page Title" />
                             </div>
                             <div class="input-group col-md-6 text-right">
                                 <button class="btn btn-default" title="Delete this Page" name="addPage"><span class="glyphicon glyphicon-trash"></span></button>
                             </div>
                         </div>
-                        <div class="panel-body">
-                            <p>
+                        <div class="panel-body custom-body">
+                            <p id="uploadPanel">
+                                <label class="fileContainer btn btn-default">
+                                    <span class="glyphicon glyphicon-upload"></span>&nbsp;Upload an Image
+                                    <input type="file" name="imgUpload" class="btn btn-success"/>
+                                </label>
                                 <strong>Upload an Image for this page (Optional)</strong>
-                                <input type="file" name="imgUpload" class="text-center" />
                             </p>
-                            <hr/>
                             <div class="input-group center-block">
-                              <textarea name="pageContent" class="form-control " placeholder="What is this page about?"></textarea>
+                              <textarea name="pageContent" class="form-control" id="textArea" placeholder="What is this page about?"></textarea>
                             </div>
                         </div>
-                        <div class="panel-footer text-right">
-                            Page 1
+                        <div class="panel-footer text-right custom-footer">
+                            <label id="pageNum">1</label>
                         </div>
                     </div>
 
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
+                    <div class="panel panel-default custom-sub-panel">
+                        <div class="panel-heading custom-heading">
                             <div class="col-md-6 pull-left center-block">
-                                <input type="text" class="form-control text-center" name="pageTitle" placeholder="Title of your Page" />
+                                <input type="text" class="form-control custom-sub-form" name="pageTitle" placeholder="Provide a Page Title" />
                             </div>
                             <div class="input-group col-md-6 text-right">
                                 <button class="btn btn-default" title="Delete this Page" name="addPage"><span class="glyphicon glyphicon-trash"></span></button>
                             </div>
                         </div>
-                        <div class="panel-body">
-                            <p>
+                        <div class="panel-body custom-body">
+                            <p id="uploadPanel">
+                                <label class="fileContainer btn btn-default">
+                                    <span class="glyphicon glyphicon-upload"></span>&nbsp;Upload an Image
+                                    <input type="file" name="imgUpload" class="btn btn-success"/>
+                                </label>
                                 <strong>Upload an Image for this page (Optional)</strong>
-                                <input type="file" name="imgUpload" class="text-center" />
                             </p>
-                            <hr/>
                             <div class="input-group center-block">
-                              <textarea name="pageContent" class="form-control " placeholder="What is this page about?"></textarea>
+                              <textarea name="pageContent" class="form-control" id="textArea" placeholder="What is this page about?"></textarea>
                             </div>
                         </div>
-                        <div class="panel-footer text-right">
-                            Page 2
+                        <div class="panel-footer text-right custom-footer">
+                            <label id="pageNum">2</label>
                         </div>
                     </div>
 
                 </div>  
-
             </div>
 
-            <div class="pull-right">
-                <button class="btn btn-primary" title="Add another Page" name="addPage"><span class="glyphicon glyphicon-plus"></span> &nbsp;Add a Page</button>
-            </div>
+            <button class="btn btn-primary pull-right" title="Add another Page" name="addPage" id="addPgBtn"><span class="glyphicon glyphicon-plus"></span>&nbsp;Add a Page</button>
+
         </form>
     </div>
 </div>
