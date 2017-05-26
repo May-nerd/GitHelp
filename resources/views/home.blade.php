47df1e9538cd6f8ce0e834117b7ff562363c94b8
linseeh <shiruba.rein15@gmail.com>
2017-05-27 02:18:10 +0800

    pulling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,13 +15,31 @@
                             @forelse(Auth::user()->creates as $lesson)
                                 <div>
                                     {{ $lesson->title }}
-                                    <form action="/lessons/{{$lesson->id}}" method="post">
-                                        {{ csrf_field() }}
-                                        {{method_field('DELETE')}}
-                                        <input type="submit" class="btn btn-danger" value="Delete"/>
-                                    </form>
+
+                                    <button class="btn btn-danger" data-toggle="modal" data-target="#deleteLesson">Delete</button>
                                 </div>
                                 <hr />
+
+                            <div class="modal fade" id="deleteLesson" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        </div>
+                                        <div class="modal-body">
+                                          <h4>Are you sure you want to delete this lesson?</h4>
+                                          
+                                            <form action="/lessons/{{$lesson->id}}" method="post">
+                                                {{ csrf_field() }}
+                                                {{method_field('DELETE')}}
+
+                                                <input type="submit" class="btn btn-danger" value="Delete"/>
+                                                <input type="button" class="btn btn-default" value="No" data-dismiss="modal">
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             @empty
                                 <h5>You don't have any lessons yet!</h5>
                             @endforelse
@@ -32,4 +50,6 @@
         </div>
     </div>
 </div>
+
+
 @endsection
