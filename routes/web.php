a8dbbe8487fceeba2f792213ff703f05bad518a6
Hope Ibelgaufts <hopibel@gmail.com>
2017-05-27 12:51:57 +0800

    Fix indentation to match laravel (4 spaces)

--- routes/web.php
+++ routes/web.php
@@ -39,30 +39,28 @@ Route::get('categories', function(){ return view('content/category'); });
 Route::resource('/lessons', 'LessonController');
 
 Route::group(['middleware' => 'auth'], function(){
-	
+    Route::get('/home', 'HomeController@show');
 
-	Route::get('/home', 'HomeController@show');
+    // This is for the tag, ajax
+    Route::get('/home/{id}', 'HomeController@index');
 
-	// This is for the tag, ajax
-	Route::get('/home/{id}', 'HomeController@index');
+    Route::get('tags/{maincategory}/{tagname}', 'TagController@index');
 
-	Route::get('tags/{maincategory}/{tagname}', 'TagController@index');
+    Route::resource('/profile', 'ProfileController');
 
-	Route::resource('/profile', 'ProfileController');
-	
-// 	Route::resource('/profile', 'ProfileController');
-// 	// Route::patch('/profile/{username}', 'ProfileController@update');
-// 	// Route::resource('/edit', 'ProfileController');
+//  Route::resource('/profile', 'ProfileController');
+//  // Route::patch('/profile/{username}', 'ProfileController@update');
+//  // Route::resource('/edit', 'ProfileController');
 
-	Route::get('/profile/{username}', 'ProfileController@profile');
-    	Route::get('/profile/{username}/subscribes', 'ProfileController@subscribe');
-    	Route::get('/profile/{username}/unsubscribes', 'ProfileController@unsubscribe');
-	Route::get('/profile/edit/{username}', 'ProfileController@edit');
-	Route::resource('/profile', 'ProfileController');
-    
-    	Route::resource('/notification', 'NotifsController');
+    Route::get('/profile/{username}', 'ProfileController@profile');
+    Route::get('/profile/{username}/subscribes', 'ProfileController@subscribe');
+    Route::get('/profile/{username}/unsubscribes', 'ProfileController@unsubscribe');
+    Route::get('/profile/edit/{username}', 'ProfileController@edit');
+    Route::resource('/profile', 'ProfileController');
 
-	Route::get('/profile/edit/{username}', 'ProfileController@edit');
-	Route::resource('/profile', 'ProfileController');
-	Route::get('/getlessons/{lesson_id}/{page_number}', 'LessonController@getPage');
+    Route::resource('/notification', 'NotifsController');
+
+    Route::get('/profile/edit/{username}', 'ProfileController@edit');
+    Route::resource('/profile', 'ProfileController');
+    Route::get('/getlessons/{lesson_id}/{page_number}', 'LessonController@getPage');
 });
